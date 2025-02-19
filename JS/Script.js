1409974844e438dd2b288974df3c16d7bcad8945
Rosa Llorente <rosamariallorentemojica.com>
2025-02-20 00:25:23 +0100

    Retoques finales y adaptación a pantalla pequeña

--- JS/Script.js
+++ JS/Script.js
@@ -28,7 +28,7 @@ window.onload = () =>
     {
         if(index < NutriaTrite.length)
         {
-            tamaño = tamaño + 2;
+            tamaño = tamaño + 1;
             img.src = NutriaTrite[index];
             Frase.textContent = ContenidoParrago[index];
             BotonSi.style.fontSize = tamaño + 'em';
@@ -47,8 +47,10 @@ window.onload = () =>
         img.src = "../Img/besoNutria.jpg";
         BotonSi.style.display='none';
         BotonNo.style.display='none';
-        Frase.innerHTML = "Chiiiiiiiiiii! Me dijo que siiiiiiiiii!<b>Te amoooooooo</b>";
-        lanzarEfectos(30);
+        Frase.innerHTML = "❤️Chiiiiiiiiiii! Me dijo que siiiiiiiiii!<b>Te amoooooooo</b>❤️";
+        lanzarEfectos(300);
+        lanzarEfectos2(50);
+        playsound();
     })
 }
 function lanzarEfectos(cantidad)
@@ -71,12 +73,50 @@ function lanzarEfectos(cantidad)
             corazon.classList.add("corazon");
             corazon.style.left = Math.random() * (window.innerWidth - 50) + "px";
             contenedor.appendChild(corazon);
-            setTimeout(() => corazon.remove(), 3000);
+            setTimeout(() => corazon.remove(), 4000);
+        },i * 200);
+    }
+}
+function lanzarEfectos2(cantidad)
+{
+    const contenedor = document.getElementById('contenedorEfectos');
+    for (let i=0; i < cantidad; i++)
+    {
+        setTimeout(() =>
+        {const flor1 = document.createElement("img");
+            flor1.src = "../Img/flor1.png"; 
+            flor1.classList.add("corazon");
+            flor1.style.left = Math.random() * (window.innerWidth - 100) + "px";
+            contenedor.appendChild(flor1);
+            setTimeout(() => flor1.remove(), 3000);
 
-        },i * 100);
+            const flor2 = document.createElement("img");
+            flor2.src = "../Img/flor2.png"; 
+            flor2.classList.add("corazon");
+            flor2.style.left = Math.random() * (window.innerWidth - 70) + "px";
+            contenedor.appendChild(flor2);
+            setTimeout(() => flor1.remove(), 3000);
+
+            const flor3 = document.createElement("img");
+            flor3.src = "../Img/flor3.png"; 
+            flor3.classList.add("corazon");
+            flor3.style.left = Math.random() * (window.innerWidth - 30) + "px";
+            contenedor.appendChild(flor3);
+            setTimeout(() => flor1.remove(), 3000);
+
+            const flor4 = document.createElement("img");
+            flor4.src = "../Img/flor4.png"; 
+            flor4.classList.add("corazon");
+            flor4.style.left = Math.random() * (window.innerWidth - 50) + "px";
+            contenedor.appendChild(flor4);
+            setTimeout(() => flor1.remove(), 3500);
+        },i * 400);
     }
 }
 function playsound()
 {
-
+    const MusicaFondo = new Audio('../Audio/Indila - Love Story (Lyrics).mp3');
+    MusicaFondo.play();
+    MusicaFondo.loop = true;
+    MusicaFondo.volume = 0.05;
 }
